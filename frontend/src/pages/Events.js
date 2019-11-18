dc0a1479494c0a13c001813db5a4b20f5f257c18
Pierre <pierre.gorde@bdlcm.com>
2019-11-18 17:07:32 +0100

    FEAT : Video 15 done

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+
+import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
+
+import "./Events.css";
 
 function EventsPage() {
-	return <h1>The Events Page</h1>;
+	const [creating, setCreating] = useState(false);
+
+	return (
+		<Fragment>
+			{creating && (
+				<Fragment>
+					<Backdrop />
+					<Modal
+						canCancel
+						canConfirm
+						onCancel={e => setCreating(false)}
+						onConfirm={() => console.log("confirm")}
+						title="Add Event"
+					>
+						<p>Modal Content</p>
+					</Modal>
+				</Fragment>
+			)}
+			<div className="events-control">
+				<p>Share your own Events !</p>
+				<button className="btn" onClick={e => setCreating(true)}>
+					Create Event
+				</button>
+			</div>
+		</Fragment>
+	);
 }
 
 export default EventsPage;
