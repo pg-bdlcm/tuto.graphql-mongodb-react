c5781260090260c70a88a96e6a9a0f5b96ee3311
Pierre <pierre.gorde@bdlcm.com>
2019-11-18 18:27:41 +0100

    FEAT : Video 16 [2:46]

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -20,7 +20,24 @@ function EventsPage() {
 						onConfirm={() => console.log("confirm")}
 						title="Add Event"
 					>
-						<p>Modal Content</p>
+						<form>
+							<div className="form-control">
+								<label htmlFor="title">Title</label>
+								<input type="text" id="title"></input>
+							</div>
+							<div className="form-control">
+								<label htmlFor="price">Price</label>
+								<input type="number" id="price"></input>
+							</div>
+							<div className="form-control">
+								<label htmlFor="date">Date</label>
+								<input type="date" id="date"></input>
+							</div>
+							<div className="form-control">
+								<label htmlFor="description">Description</label>
+								<textarea id="description" rows="4" />
+							</div>
+						</form>
 					</Modal>
 				</Fragment>
 			)}
