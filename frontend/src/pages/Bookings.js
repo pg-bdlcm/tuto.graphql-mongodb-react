7f7b6a79e83bacec7533071cb5e85e13c31dfa4f
Pierre <gordepierre@gmail.com>
2019-11-20 20:27:02 +0100

    FEAT : Video 18 done

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,7 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import Spinner from "../components/Spinner/Spinner";
+
+import AuthContext from "../context/auth-context";
 
 function BookingsPage() {
-	return <h1>The Bookings Page</h1>;
+	const [isLoading, setIsLoading] = useState(false);
+	const [bookings, setBookings] = useState([]);
+
+	const Auth = React.useContext(AuthContext);
+
+	const fetchBookings = () => {
+		setIsLoading(true);
+		/** GraphQL */
+		const requestBody = {
+			query: `
+				query {
+						bookings
+						{
+								_id
+								createdAt
+								event {
+									_id
+									title
+									date
+								}
+						}
+				}
+				`
+		};
+
+		fetch("http://localhost:4000/graphql", {
+			method: "POST",
+			body: JSON.stringify(requestBody),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${Auth.token}`
+			}
+		})
+			.then(response => {
+				if (response.status !== 200 && response.status !== 201)
+					throw new Error("Failed");
+
+				return response.json();
+			})
+			.then(data => {
+				const bookings = data.data.bookings;
+				setBookings(bookings);
+				setIsLoading(false);
+			})
+			.catch(err => {
+				console.log(err);
+				setIsLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		fetchBookings();
+	}, []);
+
+	return (
+		<React.Fragment>
+			{isLoading && <Spinner />}
+			{!isLoading && (
+				<ul>
+					{bookings.map((booking, key) => (
+						<div>
+							{booking.event.title} -{" "}
+							{new Date(booking.createdAt).toLocaleDateString()}
+						</div>
+					))}
+				</ul>
+			)}
+		</React.Fragment>
+	);
 }
 
 export default BookingsPage;
